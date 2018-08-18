214853a2c6b62dbf291944d7fc358eb275b5bb45
Wang Xiaoqiang <xq21@vip.qq.com>
2018-08-19 03:20:47 +0800

    Remove meteor client codes, because we have standalone version of react one already

--- server/tests/main.js
+++ server/tests/main.js
@@ -1,16 +1,10 @@
 import assert from "assert";
 
-describe("new-server", function () {
+describe("server", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "new-server");
+    assert.strictEqual(name, "server");
   });
-
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
 
   if (Meteor.isServer) {
     it("server is not client", function () {
